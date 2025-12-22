f3dd24cfd0795f60016a986b5513e3a1a0aa0027
manish <manishkeer530@gmail.com>
2025-12-22 13:27:38 +0530

    by default light theme done

--- src/App.js
+++ src/App.js
@@ -32,13 +32,11 @@ const App = () => {
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
     if (theme) {
       setColorMode(theme)
-    }
-
-    if (isColorModeSet()) {
       return
     }
 
-    setColorMode(storedTheme)
+    // Always set to light theme by default, or use stored theme if available
+    setColorMode(storedTheme || 'light')
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
